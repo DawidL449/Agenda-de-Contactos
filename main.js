dacd9e5926436c9341721513a8c4780e9d82a955
Yo231s <dawid.lempicki.1@gmail.com>
2022-12-20 18:05:00 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const nombre = document.querySelector (nombre)
+const numero = document.querySelector
+const direccion = document.querySelector
+const listadoTareas = document.querySelector()
+const db = window.localStorage
